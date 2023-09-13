6691705d9d30f6a0b0bbf88768bd75511020a86b
Clever2Sure <cleverpaul2sure@gmail>
2023-09-13 16:15:26 +0100

    commit update7

--- stage_two/app.py
+++ stage_two/app.py
@@ -87,4 +87,5 @@ def delete_person(person_id):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True, port=5003)  # Run the app on port 5003
+    app.run(debug=True, host='0.0.0.0', port=5003)
+    # Run the app on port 5003
